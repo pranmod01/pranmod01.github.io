11f1a3e103039943430c7730a4b13899710f15ff
pranmod01 <57882069+pranmod01@users.noreply.github.com>
2025-08-12 22:27:08 +0000

    Deploying to gh-pages from @ pranmod01/pranmod01.github.io@eb6a9edc1fe7dc3c562280ca9caf0598dce7eca7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "Blog",
+          title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
